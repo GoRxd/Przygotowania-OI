688815c0881a5fb09185a9dbc8cc8607ae523f55
GoRxd <damiansowinski15@gmail.com>
2026-01-19 21:13:51 +0100

    Graphs + DP update

---
+++ CSES/DP/array_description.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+	long long n, m;
+ 
+	cin >> n >> m;
+ 
+	vector<long long> nums (n);
+	vector<vector<long long>> dp (n + 1, vector<long long>(m + 1, 0));
+ 
+	for(long long i = 0; i < n; i++)
+	{
+		cin >> nums[i];
+	}
+ 
+	if (nums[0] == 0)
+	{
+		for(long long i = 0; i <= m; i++)
+		{
+			dp[0][i] = 1;
+		}
+	}
+	else
+		dp[0][nums[0]] = 1; // dla 2 0 3, to bedzie dp[0][2]=1
+ 
+ 
+	for(long long i = 0; i < n - 1; i++)//wysokosc drzewa
+	{
+		for(long long k = 1; k <= m; k++) //iterujemy po kazdej mozliwej kulce w danym depth
+		{
+			if (dp[i][k] != 0 && nums[i + 1] == 0)
+			{
+				for(long long j = k - 1; j <= k + 1; j++)
+				{
+					if (j >= 1 && j <= m && abs(k - j) <= 1)
+						dp[i + 1][j] = (dp[i][k] + dp[i + 1][j]) % 1'000'000'007;
+				}
+			}
+			else if (nums[i + 1] != 0)
+			{
+				for(long long j = nums[i + 1] - 1; j <= nums[i + 1] + 1; j++)
+				{
+					if (j >= 1 && j <= m)
+						dp[i + 1][nums[i + 1]] = (dp[i][j] + dp[i + 1][nums[i + 1]]) % 1'000'000'007;
+				}
+				break;
+			}
+ 
+		
+		}
+	}
+	/*
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j <= m; j++)
+		{
+			if (dp[i][j] != 0)
+				printf("dp[%d][%d]: %d\n", i, j, dp[i][j]);
+		}
+	}
+	*/
+	
+	long long ways = 0;
+ 
+	for(int k = 1; k <= m; k++)
+	{
+		ways = (ways + dp[n - 1][k]) % 1'000'000'007;
+	}
+ 
+	cout << ways << endl;
+}
---
+++ CSES/Graphs/counting_rooms.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+struct Pos
+{
+	int y;
+	int x;
+	Pos(int y, int x)
+	{
+		this->y = y;
+		this->x = x;
+	}
+};
+
+static void BFS(int y, int x, vector<vector<bool>>& visited, vector<vector<char>>& map)
+{
+	queue<Pos> q;
+
+	q.push(Pos(y, x));
+	visited[y][x] = true;
+
+	while(!q.empty())
+	{
+		Pos curr = q.front();
+		q.pop();
+
+		if (curr.y + 1 < map.size() && curr.x >= 0 && curr.x < map[0].size()) //top
+		{
+			if (map[curr.y + 1][curr.x] != '#' && !visited[curr.y + 1][curr.x])
+			{
+				q.push(Pos(curr.y + 1, curr.x));
+				visited[curr.y + 1][curr.x] = true;
+			}
+		}
+
+		if (curr.y - 1 >= 0 && curr.x >= 0 && curr.x < map[0].size()) // bottom
+		{
+			if (map[curr.y - 1][curr.x] != '#' && !visited[curr.y - 1][curr.x])
+			{
+				q.push(Pos(curr.y - 1, curr.x));
+				visited[curr.y - 1][curr.x] = true;
+			}
+		}
+
+		if (curr.x + 1 < map[0].size() && curr.y < map.size() && curr.y >= 0)
+		{
+			if (map[curr.y][curr.x + 1] != '#' && !visited[curr.y][curr.x + 1])
+			{
+				q.push(Pos(curr.y, curr.x + 1));
+				visited[curr.y][curr.x + 1] = true;
+			}
+		}
+
+		if (curr.x - 1 >= 0 && curr.y < map.size() && curr.y >= 0)
+		{
+			if (map[curr.y][curr.x - 1] != '#' && !visited[curr.y][curr.x - 1])
+			{
+				q.push(Pos(curr.y, curr.x - 1));
+				visited[curr.y][curr.x - 1] = true;
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m, rooms=0;
+
+	cin >> n >> m;
+
+	vector<vector<bool>> visited (n, vector<bool>(m));
+
+	vector<vector<char>> map (n, vector<char>(m));
+
+	for(int y = 0; y < n; y++)
+	{
+		for(int x = 0; x < m; x++)
+		{
+			cin >> map[y][x];
+		}
+	}
+
+	for(int y = 0; y < n; y++)
+	{
+		for(int x = 0; x < m; x++)
+		{
+			if (!visited[y][x] && map[y][x] != '#')
+			{
+				BFS(y, x, visited, map);
+				rooms++;
+			}
+		}
+	}
+
+	cout << rooms << endl;
+}
+
